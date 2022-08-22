c5ea00c81aba960fb38a97758bedcfbaa76f0b89
이빛나 <111333407+LEEbinna@users.noreply.github.com>
2022-08-22 09:10:18 +0900

    Update Program.cs

--- 01_Console/01_Console/Program.cs
+++ 01_Console/01_Console/Program.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace _01_Console
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class Orc   //오크 설계 
         {
-            Console.WriteLine("Hello World!");  //  Hello World!를 출력하는 코드
-            Console.ReadKey();                  // 키 입력 대기하는 코드
-            
+            int HP = 20;
+            int MaxHP = 20;
+            int ATT = 5;
+            int DEF = 5;
         }
+
+        public class Human    //인간 설계
+        {
+            int HP = 100;
+            int MaxHP = 100;
+            int ATT = 10;
+            int DEF = 10;
+        }
+
+        
+
+        static void Main(string[] args) //함수
+        {
+            // 주말과제용
+            Human player = new Human();
+            Orc enemy = new Orc();
+
+            public void Attack(Character01 target)
+            {
+                int damage = ;
+
+                //rand.NextDouble();  // 0.0 ~ 1.0
+                if (rand.NextDouble() < 0.3f)   // 이 조건이 참이면 30% 안쪽으로 들어왔다.
+                {
+                    damage *= 2;    // damage = damage * 2;
+                    Console.WriteLine("크리티컬 히트!");
+                }
+
+                Console.WriteLine($"{name}이 {target.Name}에게 공격을 합니다.(공격력 : {damage})");
+                target.TakeDamage(damage);
+            }
+            Console.ReadKey();
+        }   
     }
 }
