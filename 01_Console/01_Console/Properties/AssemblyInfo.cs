83d328e26fd828a1f3e60db5d13f3931d11523a1
이빛나 <111333407+LEEbinna@users.noreply.github.com>
2022-08-29 05:35:12 +0900

    shooter- animatior

--- 01_Console/01_Console/Properties/AssemblyInfo.cs
+++ 01_Console/01_Console/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("4548da10-f727-4b4e-8033-ef9dbdfe56de")]
+[assembly: Guid("e4372628-f461-427e-a451-add4c8853bc3")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
---
+++ 02_Shooting/Assets/Scripts/Player.cs
@@ -0,0 +1,160 @@
+//using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    //public delegate void DelegateName();    // 이런 종류의 델리게이트가 있다 (리턴없고 파라메터도 없는 함수를 저장하는 델리게이트)
+    //public DelegateName del;      // DelegateName 타입으로 del이라는 이름의 델리게이트를 만듬
+    //Action del2;                  // 리턴타입이 void, 파라메터도 없는 델리게이트 del2를 만듬
+    //Action<int> del3;             // 리턴타입이 void, 파라메터는 int 하나인 델리게이트 del3을 만듬
+    //Func<int, float> del4;        // 리턴타입이 int고 파라메터는 float 하나인 델리게이트 del4를 만듬
+
+    public float speed = 1.0f;      // 플레이어의 이동 속도(초당 이동 속도)
+    public GameObject bullet;
+
+    Vector3 dir;                    // 이동 방향(입력에 따라 변경됨)
+    float boost = 1.0f;
+
+    Rigidbody2D rigid;
+    Animator anim;
+
+    PlayerInputAction inputActions;
+    // Awake -> OnEnable -> Start : 대체적으로 이 순서
+
+    /// <summary>
+    /// 이 스크립트가 들어있는 게임 오브젝트가 생성된 직후에 호출
+    /// </summary>
+    private void Awake()
+    {
+        inputActions = new PlayerInputAction();
+        rigid = GetComponent<Rigidbody2D>();    // 한번만 찾고 저장해서 계속 쓰기(메모리 더 쓰고 성능 아끼기)
+        anim = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// 이 스크립트가 들어있는 게임 오브젝트가 활성화 되었을때 호출
+    /// </summary>
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();   // 오브젝트가 생성되면 입력을 받도록 활성화
+        inputActions.Player.Move.performed += OnMove;   // Move액션이 performed 일 때 OnMove 함수 실행하도록 연결
+        inputActions.Player.Move.canceled += OnMove;    // Move액션이 canceled 일 때 OnMove 함수 실행하도록 연결
+        inputActions.Player.Fire.performed += OnFire;
+        inputActions.Player.Boost.performed += OnBoostOn;
+        inputActions.Player.Boost.canceled += OnBoostOff;
+    }
+
+    /// <summary>
+    /// 이 스크립트가 들어있는 게임 오브젝트가 비활성화 되었을 때 호출
+    /// </summary>
+    private void OnDisable()
+    {
+        inputActions.Player.Fire.performed -= OnFire;
+        inputActions.Player.Move.canceled -= OnMove;    // 연결해 놓은 함수 해제(안전을 위해)
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Disable();  // 오브젝트가 사라질때 더 이상 입력을 받지 않도록 비활성화
+    }
+
+    /// <summary>
+    /// 시작할 때. 첫번째 Update 함수가 실행되기 직전에 호출.
+    /// </summary>
+    private void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// 매 프레임마다 호출.
+    /// </summary>
+    //private void Update()
+    //{
+    //    //transform.position += (speed * Time.deltaTime * dir);
+    //    //transform.Translate(speed * Time.deltaTime * dir);
+    //    //transform.Translate(speed * Time.deltaTime * dir.x, speed * Time.deltaTime * dir.y, 0);
+
+    //    //transform.position = dir;
+    //}
+
+    /// <summary>
+    /// 일정 시간 간격(물리 업데이트 시간 간격)으로 호출
+    /// </summary>
+    private void FixedUpdate()
+    {
+        //transform.Translate(speed * Time.fixedDeltaTime * dir);
+
+        // 이 스크립트 파일이 들어 있는 게임 오브젝트에서 Rigidbody2D 컴포넌트를 찾아 리턴.(없으면 null)
+        // 그런데 GetComponent는 무거운 함수 => (Update나 FixedUpdate처럼 주기적 또는 자주 호출되는 함수 안에서는 안쓰는 것이 좋다)
+        // Rigidbody2D rigid = GetComponent<Rigidbody2D>();    
+
+        // rigid.AddForce(speed * Time.fixedDeltaTime * dir); // 관성이 있는 움직임을 할 때 유용
+        rigid.MovePosition(transform.position + boost * speed * Time.fixedDeltaTime * dir); // 관성이 없는 움직임을 처리할 때 유용
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionEnter2D");    // Collider와 부딪쳤을 때 실행
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionStay2D");     // Collider와 계속 접촉하면서 움직일 때 (매 프레임마다 호출)
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionExit2D");     // Collider와 접촉이 떨어지는 순간 실행
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerEnter2D");      // 트리거에 들어갔을 때 실행
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerStay2D");       // 트리거와 계속 겹쳐있으면서 움직일 때 (매 프레임마다 호출)
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerExit2D");       // 트리거에서 나갔을 때 실행
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        // Exception : 예외 상황( 무엇을 해야 할지 지정이 안되어있는 예외 일때 )
+        //throw new NotImplementedException();    // NotImplementedException 을 실행해라. => 코드 구현을 알려주기 위해 강제로 죽이는 코드
+
+        //Debug.Log("이동 입력");
+        dir = context.ReadValue<Vector2>();    // 어느 방향으로 움직여야 하는지를 입력받음
+
+        //dir.y > 0   // W를 눌렀다.
+        //dir.y == 0  // W,S 중 아무것도 안눌렀다.
+        //dir.y < 0   // S를 눌렀다.
+        anim.SetFloat("InputY", dir.y);
+
+    }
+
+    private void OnFire(InputAction.CallbackContext context)
+    {
+        //Debug.Log("발사!");
+        //float value = Random.Range(0.0f, 10.0f);  // value에는 0.0 ~ 10.0의 랜덤값이 들어간다.
+
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+
+    private void OnBoostOn(InputAction.CallbackContext context)
+    {
+        boost *= 2.0f;
+    }
+
+    private void OnBoostOff(InputAction.CallbackContext context)
+    {
+        boost = 1.0f;
+    }
+
+
+}
